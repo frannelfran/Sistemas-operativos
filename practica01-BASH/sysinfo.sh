d4ef6766ebf6fd2cb3a329070ed3700776883056
usuario <alu0101571669@ull.edu.es>
2023-10-05 10:44:54 +0000

    05-10-2023

---
+++ practica01-BASH/sysinfo.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Práctica de Sistemas Operativos
+# Un script que informa del estado del sistema
+
+##### Constantes
+
+TITLE="Información del sistema para $HOSTNAME"
+RIGHT_NOW=$(date +"%x %r%Z")
+TIME_STAMP="Actualizada el $RIGHT_NOW por $USER"
+
+##### Estilos
+
+TEXT_BOLD=$(tput bold) # Letra en negrita
+TEXT_GREEN=$(tput setaf 2) # Letra verde
+TEXT_RESET=$(tput sgr0) # Quitar el formato
+TEXT_ULINE=$(tput sgr 0 1) # Subraya el texto
+
+##### Funciones
+
+system_info() {
+  echo "${TEXT_ULINE}Versión del sistema${TEXT_RESET}"
+  echo # Nueva línea
+  uname -a # Muestra la versión del sistema
+}
+
+show_uptime() {
+  echo "${TEXT_ULINE}Tiempo de encendido del sistema${TEXT_RESET}"
+  echo # Nueva línea
+  uptime
+}
+
+drive_space()
+{
+  echo "Función drive_space"
+}
+
+home_space()
+{
+  echo "Función home_space"
+}
+
+##### Programa principal
+
+cat << _EOF_
+$TEXT_BOLD$TITLE$TEXT_RESET
+$(system_info)
+
+$(show_uptime)
+
+$(drive_space)
+
+$(home_space)
+$TEXT_GREEN$TIME_STAMP$TEXT_RESET
+_EOF_
