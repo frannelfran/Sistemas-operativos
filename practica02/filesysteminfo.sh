b0e8cdadba6362cc20db96c468a2360cb977c55d
usuario <alu0101571669@ull.edu.es>
2023-10-17 10:56:12 +0000

    17/10/2023

--- practica02/filesysteminfo.sh
+++ practica02/filesysteminfo.sh
@@ -1,21 +1,39 @@
-# VARIABLES
-TITLE="Programa creado por $USER"
+#!/bin/bash
 
-# ESTILOS
-TEXT_BOLD=$(tput bold) # Letra en negrita
-TEXT_GREEN=$(tput setaf 2) # Letra verde
-TEXT_RESET=$(tput sgr0) # Quitar el formato
-TEXT_ULINE=$(tput sgr 0 1) # Subraya el texto
-
-# FUNCIONES
+option=$1
 help() {
   echo "Uso: scdebug [-h] [-sto arg] [-v | -vall] [-nattch progtoattach] [prog [arg1 …]]"
+  exit 0
 }
 
-# PROGRAMA PRINCIPAL
-cat << salida
-$TITLE
+run_strace() {
+  local program="$1"
+  local output_dir=".scdebug/$program"
+  local output_file="$output_dir/trace_$(uuidgen).txt"
+  mkdir -p "$output_dir"
+  shift
+  strace "$@" -o "$output_file" "$program"
+  if [ $? -ne 0 ]; then
+    echo "Error al ejecutar strace para $program." >&2
+    exit 1
+  fi
+  echo "strace ha finalizado para $program. Resultado en $output_file"
+}
 
+# Menú de opciones
+case $option in
+	-h)
+		help
+	;;
+	-sto)
+		sto_option="$2"
+		shift 2
+	;;
+esac
 
-
-salida
+# Ejecutar el strace con las opciones marcadas
+if [ -n "$sto_option" ]; then
+  run_strace "$1" $sto_option
+else
+  run_strace "$1" "$@"
+fi
