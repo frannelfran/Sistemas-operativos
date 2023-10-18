8ce6b528e3b18263400779af32563f49f9fa8f6f
usuario <alu0101571669@ull.edu.es>
2023-10-18 15:22:23 +0000

    18/16/2023

--- practica02/scdebug.sh
+++ practica02/scdebug.sh
@@ -26,26 +26,25 @@ run_strace() {
 attach_strace() {
   program="$1"
   options=("${@:2}")
+  # MODIFICACIÓN
+  # Obtener la lista de PIDs
+  pid_list=($(pgrep -u $USER "$program"))
 
-  # Obtener el PID del proceso más reciente
-  latest_pid=$(pgrep -u $USER "$program" | tail -n 1)
-
-  if [ -z "$latest_pid" ]; then
+  if [ "${#pid_list[@]}" -eq 0 ]; then
     echo "No se encontraron procesos de $program en ejecución."
     return
   fi
 
-# Modificación
-  echo "Proceso $program más reciente encontrado (PID $latest_pid)."
-  run_strace "$program" "-p $latest_pid" "${options[@]}"
+  # Mostrar la lista de PIDs
   echo "Lista de PIDs para $program:"
   for pid in "${pid_list[@]}"; do
     echo "  $pid"
   done
+
   read -p "Elija el PID al que desea enganchar strace: " selected_pid
 
-  # Revisar que el PID esta dentro de las opciones
-  if [[ ! " ${pid_list[@]} " =~ " ${selected_pid} " ]]; then
+  # Revisar que el PID está dentro de las opciones
+  if [[ ! " ${pid_list[*]} " =~ " $selected_pid " ]]; then
     echo "PID no válido. Saliendo."
     exit 1
   fi
@@ -54,34 +53,35 @@ attach_strace() {
   run_strace "$program" "-p $selected_pid" "${options[@]}"
 }
 
-  program="$1"
-  args=("${@:2}")
-  sto_option=""
-  output_dir="$HOME/.scdebug/$program"
-  uuid=$(uuidgen)
+# Inicializar variables
+program="$1"
+args=("${@:2}")
+sto_option=""
+output_dir="$HOME/.scdebug/$program"
+uuid=$(uuidgen)
 
-  # Función de ayuda
-  help() {
-    echo "scdebug [-h] [-sto arg] [-v | -vall] [-nattch progtoattach] [prog [arg1 …]]"
-    exit 1
-  }
+# Función de ayuda
+help() {
+  echo "scdebug [-h] [-sto arg] [-v | -vall] [-nattch progtoattach] [prog [arg1 …]]"
+  exit 1
+}
 
-  # Dependiendo de la opcion que se elija se hace una cosa u otra
+# Dependiendo de la opción que se elija se hace una cosa u otra
 case "$1" in
   -h)
     help
-  ;;
+    ;;
   -sto)
     sto_option="$2"
     shift 2
-  ;;
+    ;;
   -nattch)
     attach_strace "$2" $sto_option
     exit 0
-  ;;
+    ;;
   *)
     break
     ;;
-  esac
+esac
 
 run_strace "$program" "${args[@]}" $sto_option
