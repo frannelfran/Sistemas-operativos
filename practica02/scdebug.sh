6dabcd5219c53ed302adef596f4d3b6f939863a6
usuario <alu0101571669@ull.edu.es>
2023-10-18 11:47:26 +0000

    18/10/2023

--- practica02/scdebug.sh
+++ practica02/scdebug.sh
@@ -20,6 +20,28 @@ run_strace() {
   echo "strace ha finalizado para $program. Resultado en $output_file"
 }
 
+attach_strace() {
+  local program_name="$1"
+  local output_dir=".scdebug/$program_name"
+  local output_file="$output_dir/trace_$(uuidgen).txt"
+  mkdir -p "$output_dir"
+
+  # Obtener el PID del proceso más reciente con el nombre del programa
+  local pid
+  pid=$(pgrep -n -f "$program_name")
+
+  if [ -z "$pid" ]; then
+    echo "No se encontraron procesos en ejecución con el nombre '$program_name'."
+    exit 1
+  fi
+
+  strace -c -e file -p "$pid" -o "$output_file" &
+  local strace_pid=$!
+
+  echo "strace está monitoreando el proceso $pid en segundo plano."
+  echo "Resultado en $output_file"
+}
+
 # Menú de opciones
 case $option in
 	-h)
@@ -29,6 +51,13 @@ case $option in
 		sto_option="$2"
 		shift 2
 	;;
+  -nattch)
+  if [ -n "$sto_option" ]; then
+    attach_strace "$1"
+  else
+    attach_strace "$1"
+  fi
+  ;;
 esac
 
 # Ejecutar el strace con las opciones marcadas
