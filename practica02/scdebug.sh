dbef036e199bd51c90a83bc56a5d12f561629783
usuario <alu0101571669@ull.edu.es>
2023-10-29 12:41:12 +0000

    29/10/2023

--- practica02/scdebug.sh
+++ practica02/scdebug.sh
@@ -1,87 +1,76 @@
 #!/bin/bash
 
-# Función para ejecutar strace
-run_strace() {
-  program="$1"
-  args=("${@:2}")
-  options=("-o" "$output_dir/trace_$uuid.txt")
-
-  # Crear el directorio de salida si no existe
-  mkdir -p "$output_dir"
-
-  strace "${options[@]}" "$program" "${args[@]}" &
-  strace_pid=$!
-
-  wait "$strace_pid"
-
-  if [ "$?" -ne 0 ]; then
-    echo "Error al ejecutar strace."
-    exit 1
-  fi
-
-  echo "Strace en segundo plano en ejecución, salida en $output_dir/trace_$uuid.txt"
-}
-
-# Función para hacer attach a un proceso
-attach_strace() {
-  program="$1"
-  options=("${@:2}")
-  # MODIFICACIÓN
-  # Obtener la lista de PIDs
-  pid_list=($(pgrep -u $USER "$program"))
-
-  if [ "${#pid_list[@]}" -eq 0 ]; then
-    echo "No se encontraron procesos de $program en ejecución."
-    return
-  fi
-
-  # Mostrar la lista de PIDs
-  echo "Lista de PIDs para $program:"
-  for pid in "${pid_list[@]}"; do
-    echo "  $pid"
-  done
-
-  read -p "Elija el PID al que desea enganchar strace: " selected_pid
-
-  # Revisar que el PID está dentro de las opciones
-  if [[ ! " ${pid_list[*]} " =~ " $selected_pid " ]]; then
-    echo "PID no válido. Saliendo."
-    exit 1
-  fi
-
-  echo "Enganchando strace al proceso con PID $selected_pid..."
-  run_strace "$program" "-p $selected_pid" "${options[@]}"
-}
-
-# Inicializar variables
-program="$1"
-args=("${@:2}")
-sto_option=""
-output_dir="$HOME/.scdebug/$program"
-uuid=$(uuidgen)
-
-# Función de ayuda
 help() {
-  echo "scdebug [-h] [-sto arg] [-v | -vall] [-nattch progtoattach] [prog [arg1 …]]"
-  exit 1
+  echo "scdebug [-h] [-sto opciones] [-v | -vall] [-nattch progtoattach] [prog [arg1 ...]]"
+  echo "-h: Muestra este mensaje de ayuda."
+  echo "-sto opciones: Opciones personalizadas para strace (encerradas entre comillas)."
+  echo "-nattch progtoattach: Monitoriza un proceso existente por nombre."
 }
 
-# Dependiendo de la opción que se elija se hace una cosa u otra
-case "$1" in
+run_strace() {
+  local prog="$1"
+  shift
+  local args=("$@")
+  local base_dir="$HOME/.scdebug"
+  local uuid=$(uuidgen)
+  local program_dir="$base_dir/$prog"
+  if [ ! -d "$program_dir" ]; then
+    mkdir -p "$program_dir"
+  fi
+  local output_file="$program_dir/trace_$uuid.txt"
+  local strace_command="strace -o $output_file $prog ${args[@]}"
+  $strace_command &
+  local strace_pid=$!
+  wait $strace_pid
+  if [ $? -ne 0 ]; then
+    echo "Error: strace ha producido un error. Consulta el archivo $output_file para más detalles."
+    exit 1
+  else
+    echo "Ejecución exitosa. Los resultados se guardan en $output_file."
+  fi
+}
+
+attach_strace() {
+  local progtoattach="$1"
+  local pid=$(pgrep -o "$progtoattach")
+  if [ -z "$pid" ]; then
+    echo "No se encontró un proceso en ejecución con el nombre: $progtoattach."
+    exit 1
+  fi
+  local base_dir="$HOME/.scdebug"
+  local uuid=$(uuidgen)
+  local program_dir="$base_dir/$progtoattach"
+  if [ ! -d "$program_dir" ]; then
+    mkdir -p "$program_dir"
+  fi
+  local output_file="$program_dir/trace_$uuid.txt"
+  local strace_command="strace -o $output_file -p $pid"
+  $strace_command &
+  local strace_pid=$!
+  if [ $? -ne 0 ]; then
+    echo "Error: strace ha producido un error. Consulta el archivo $output_file para más detalles."
+    exit 1
+  else
+    echo "Ejecución exitosa en modo attach. Los resultados se guardan en $output_file."
+  fi
+}
+
+opcion="$1"
+case "$opcion" in
   -h)
     help
     ;;
   -sto)
-    sto_option="$2"
-    shift 2
+    shift
+    strace_options="$1"
+    shift
     ;;
-  -nattch)
-    attach_strace "$2" $sto_option
-    exit 0
-    ;;
+  -nattach)
+    shift
+    progtoattach="$1"
+    attach_strace "$progtoattach"
+  ;;
   *)
-    break
-    ;;
+    run_strace "$@"
+  ;;
 esac
-
-run_strace "$program" "${args[@]}" $sto_option
