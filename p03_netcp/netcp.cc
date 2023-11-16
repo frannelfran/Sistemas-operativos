a42404ef250abc375d46a4d70638fa61ecf7a516
usuario <alu0101571669@ull.edu.es>
2023-11-16 18:00:58 +0000

    16/11/2023

---
+++ p03_netcp/netcp.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  if (argc == 2) {
+    string ayuda = argv[1];
+    if(ayuda == "--help" || ayuda == "-h" ) {
+      cout << "Modo de empleo: ./netcp testfile " << endl;
+      exit(EXIT_SUCCESS);
+    }
+  }
+  if (argc != 2) {
+    cout << "Pruebe ‘TM_simulator [-h | --help]’ para más información." << endl;
+    return 1;
+  }
+  
+
+
+
+
+
+
+
+
+  return 0;
+}
